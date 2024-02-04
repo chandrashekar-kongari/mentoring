25ad8ff428bde736392c26ab163e64ea6e9a3990
kongarichandrashekar8 <kongarichandrashekar8@gmail.com>
2024-02-04 10:36:27 -0500

    testing

--- src/API/api.js
+++ src/API/api.js
@@ -4,7 +4,7 @@
 const endpoint =process.env.REACT_APP_BACKEND_ENDPOINT;
 //above one is working
 // const endpoint='https://dev.techpact.org/flaskapp'
-//const endpoint='http://127.0.0.1:5000'
+// const endpoint='http://127.0.0.1:5000'
 // const endpoint='https://us-central1-notification-services-a7297.cloudfunctions.net/api/'
 
 export default endpoint
